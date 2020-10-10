e1d7c058df748ef7b6fbe6806349a79bb64dd689
Pranav <pranav2000joglekar@gmail.com>
2020-10-10 11:43:46 +0530

    feat: move internal functions to lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,3 @@
+pub fn get_command(args: &[String]) -> String {
+    "hello world".to_string()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let command_to_execute = tsh::get_command(&args);
+    println!("{}", command_to_execute);
 }
