dfe4c5bde3c2585b83ae3c8e0a26bdda51816b7c
Pranav <pranav2000joglekar@gmail.com>
2020-10-10 12:48:43 +0530

    feat: add enum type to handle help command and error

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,27 @@
 pub enum CmdResult {
     EntryAdded(String),
     CmdExtracted,
+    DisplayHelp(String),
 }
 
 pub fn get_command(args: &[String]) -> CmdResult {
-    //"hello world".to_string()
+    let command = &args[1];
+    println!("{}", command);
     CmdResult::EntryAdded("hello world".to_string())
 }
+
+/*
+pub add_entry() -> {
+    // Check if the 2nd argument is add
+
+    // Extract name to be given to entry
+
+    // create /tsh directory if not exist
+
+    // create /tsh/<name> folder. error if already exists
+
+    // copy key from given path to this directory
+
+    // add endpoint to a txt file
+}
+*/
--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,14 @@ fn main() {
     let command_to_execute = tsh::get_command(&args);
     //println!("{}", command_to_execute);
     match command_to_execute {
-        tsh::CmdResult::EntryAdded(message) => {
+        tsh::CmdResult::EntryAdded(_message) => {
             println!("entry added");
         }
         tsh::CmdResult::CmdExtracted => {
             println!("ssh command rcvd");
+        }
+        tsh::CmdResult::DisplayHelp(_message) => {
+            println!("Display help output here");
         }
     }
 }
