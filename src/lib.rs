1b22621bd30a8645d5581a5923977d1233b02bdd
Pranav <pranav2000joglekar@gmail.com>
2020-10-10 12:11:08 +0530

    feat: boilerplate for handling entry addition

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
-pub fn get_command(args: &[String]) -> String {
-    "hello world".to_string()
+pub enum CmdResult {
+    EntryAdded(String),
+    CmdExtracted,
+}
+
+pub fn get_command(args: &[String]) -> CmdResult {
+    //"hello world".to_string()
+    CmdResult::EntryAdded("hello world".to_string())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
 use std::env;
 
+// tsh add <myname> endpoint <key-location>
+// tsh myname
 fn main() {
     let args: Vec<String> = env::args().collect();
     let command_to_execute = tsh::get_command(&args);
-    println!("{}", command_to_execute);
+    //println!("{}", command_to_execute);
+    match command_to_execute {
+        tsh::CmdResult::EntryAdded(message) => {
+            println!("entry added");
+        }
+        tsh::CmdResult::CmdExtracted => {
+            println!("ssh command rcvd");
+        }
+    }
 }
